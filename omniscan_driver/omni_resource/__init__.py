9a1883f20fd6c5922754d826dbd5d25766530737
sai4aax <k.sai@aquaairx.com>
2025-07-07 11:11:31 +0530

    removed package import error in init file of omni_resouces

--- omniscan_driver/omni_resource/__init__.py
+++ omniscan_driver/omni_resource/__init__.py
@@ -3,6 +3,6 @@
 from omni_resource.definitions import *
 from omni_resource.pingmessage import *
 from omni_resource.device import PingDevice
-import omniscan.mock_sensor
+import omniscan_driver.mock_sensor
 from omni_resource.device4omniscan import Device4Omniscan
 
