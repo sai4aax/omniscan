1b45bf091cba0921d868d7bd07549940a5ab461c
sai4aax <k.sai@aquaairx.com>
2025-07-07 10:15:16 +0530

    package name corrected at import

--- omniscan_driver/omniscan_driver/publish.py
+++ omniscan_driver/omniscan_driver/publish.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from omniscan.msg import OmniscanPing
+from omniscan_msgs.msg import OsMonoProfile
 from omni_resource import Device4Omniscan
 from random import uniform, randint
 import time
@@ -10,23 +10,27 @@ class OmniscanNode(Node):
         super().__init__('omniscan_driver_node')
 
         # Create publisher
-        self.publisher_ = self.create_publisher(OmniscanPing, 'omniscan/ping', 10)
+        self.publisher_ = self.create_publisher(OsMonoProfile, 'omniscan/ping', 10)
 
         # 2ms timer = 0.002 seconds => 500Hz
         self.timer = self.create_timer(0.002, self.timer_callback)
 
         self.get_logger().info('Omniscan node initialized and publishing at 2ms intervals')
+        self.start_time = time.time()
+        self.ping_number = 0
+        
 
     def timer_callback(self):
+        self.ping_number += 1
         # You can use real device data here if needed
-        msg = OmniscanPing()
-        msg.ping_number = randint(0, 1000)
+        msg = OsMonoProfile()
+        msg.ping_number = self.ping_number
         msg.start_mm = 0
         msg.length_mm = 5000
-        msg.timestamp_ms = int(time.time() * 1000)
+        msg.timestamp_ms = int((time.time() - self.start_time) * 1000)
         msg.ping_hz = 400000
         msg.gain_index = randint(0, 10)
-        msg.num_results = 5
+        msg.num_results = 10
         msg.sos_dmps = 13900
         msg.channel_number = 1
         msg.reserved = 0
